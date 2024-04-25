d175eddb7143d040e697cb497106045781a501ab
Ruba <168135526+yona123werg@users.noreply.github.com>
2024-04-26 01:57:36 +0300

    Update comm.php

--- comm.php
+++ comm.php
@@ -5,9 +5,26 @@
     <link rel="stylesheet" type="text/css" href="styles.css">
     <link rel="stylesheet" type="text/css" href="home.css">
     <link rel="stylesheet" type="text/css" href="meno.css">
+    <link rel="shortcut icon" href="قلب الرياض.jpg" type="image/x-icon"/>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body>
+    <header>
+        <img id="logl" src="قلب الرياض.png" width="150" height="150">
+    <nav>
+        <ul>
+          <li><a href="admin.html" ><i class="fa-solid fa-lock"></i></a></li>
+           <li><a href="home.HTML"><i class="fa-solid fa-house"></i></a></li>
+          <li><a href="فنادق ومنتجعات.html" data-i18n="Hotels">فنادق ومنتجعات</a></li>
+          <li><a href="مطاعم.html" data-i18n="Restaurants" >مطاعم</a></li>
+          <li><a href="أسواق.html" data-i18n="Markets">أسواق</a></li>
+          <li><a href="الثقافه والتراث.html" data-i18n="Culture">الثقافة والتراث</a></li>
+          <li><a href="الفعاليات والتجارب.html" data-i18n="Events">الفعاليات والتجارب</a></li>
+        </ul>
+    </nav>
+</header>
     <h1>تعليقات المستخدمين</h1>
     <div id="comments"></div>
     <form id="commentForm" class="input-group">
